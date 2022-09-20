c2ce7be749c8f021b6acc8e4454d9b77d53856e7
DreamTecHunter <Tobias.laser@ymail.com>
2022-09-20 11:03:16 +0200

    commit suicide

--- spl_1-01/Steganography_bitmaps_template.py
+++ spl_1-01/Steganography_bitmaps_template.py
@@ -1,6 +1,7 @@
 # tkinter provides GUI objects and commands
 import tkinter as tk
 import tkinter.ttk as ttk
+from ast import literal_eval
 # math provides some functions (ceil, floor)
 import math
 # Python Imaging Library (PIL) provides commands
@@ -13,10 +14,12 @@ root = tk.Tk()
 root.title("Steganography with bitmaps")
 root.wm_state("zoomed")
 
+
 # The labels used to interact with the user are cleared.
 def ClearFeedbackLabels():
     LabelSecretFeedback["text"] = ""
-    LabelModeFeedback["text"] = ""        
+    LabelModeFeedback["text"] = ""
+
 
 # This function is invoked when the user clicks the button
 # "Load secret from file".
@@ -27,7 +30,7 @@ def ClearFeedbackLabels():
 def ButtonSecretLoadClick():
     ClearFeedbackLabels()
     try:
-        with open(PathSecret.get(), mode = "rt", encoding = "utf-8") as tf:
+        with open(PathSecret.get(), mode="rt", encoding="utf-8") as tf:
             secret = tf.read()
     except:
         LabelSecretFeedback["text"] = "An error occurred while reading the file."
@@ -39,6 +42,7 @@ def ButtonSecretLoadClick():
             LabelSecretFeedback["text"] = "File loaded successfully."
         TextSecret.delete("1.0", "end")
         TextSecret.insert("1.0", secret)
+
 
 # This function is invoked when the user clicks the button
 # "Save secret to file".
@@ -54,7 +58,7 @@ def ButtonSecretSaveClick():
         LabelSecretFeedback["text"] = "Nothing to save"
         return
     try:
-        with open(PathSecret.get(), mode = "wt", encoding = "utf-8") as tf:
+        with open(PathSecret.get(), mode="wt", encoding="utf-8") as tf:
             if (tf.write(secret) != len(secret)):
                 raise Exception
     except:
@@ -62,12 +66,14 @@ def ButtonSecretSaveClick():
     else:
         LabelSecretFeedback["text"] = "Secret saved successfully."
 
+
 # This function is invoked by ButtonModeHideClick()
 # after the secret was hidden successfully.
 ###### ENTER YOUR CODE HERE ######
 def PrintImageComparison(ImageDataOffset):
     TextMode.delete("1.0", "end")
     pass
+
 
 # The following code lines try to display both
 # bitmaps. They are not necessary for the program
@@ -96,7 +102,19 @@ def PrintImageComparison(ImageDataOffset):
 ###### ENTER YOUR CODE HERE ######
 def ButtonModeHideClick():
     ClearFeedbackLabels()
-    pass
+    try:
+        file = open("Galaxien.bmp", "rb")
+        file_content = list(file.read())
+    except:
+        print("sth happend!!!!")
+    if file_content[0] != 66 and file_content[1] != 77:
+        print("PROBLEM")
+    print(file_content[0:1])
+    print(file_content[2:3])
+    print(file_content[4:9])
+    bf_off_bits = file_content[10:14]
+    print(bf_off_bits)
+
 
 # This function is invoked when the user presses
 # the button "Disclose secret from image".
@@ -104,75 +122,75 @@ def ButtonModeHideClick():
 def ButtonModeDiscloseClick():
     ClearFeedbackLabels()
     pass
-        
+
 
 # The window is divided into three frames.
-FrameSecret = ttk.Frame(master = root)
+FrameSecret = ttk.Frame(master=root)
 FrameSecret["borderwidth"] = 5
 FrameSecret["relief"] = "sunken"
-FrameMode = ttk.Frame(master = root)
+FrameMode = ttk.Frame(master=root)
 FrameMode["borderwidth"] = 5
 FrameMode["relief"] = "sunken"
-FrameImage = ttk.Frame(master = root)
+FrameImage = ttk.Frame(master=root)
 FrameImage["borderwidth"] = 5
 FrameImage["relief"] = "sunken"
-FrameSecret.pack(side = "left", fill = "both", expand = True)
-FrameMode.pack(side = "left", fill = "y")
-FrameImage.pack(side = "left", fill = "both", expand = True)
+FrameSecret.pack(side="left", fill="both", expand=True)
+FrameMode.pack(side="left", fill="y")
+FrameImage.pack(side="left", fill="both", expand=True)
 
 # The labels, entries, buttons and text fields
 # are defined and adjusted.
-LabelSecretCaption = ttk.Label(master = FrameSecret, text = "Secret text")
-LabelSecretCaption.pack(side = "top", pady = 5)
-PathSecret = tk.StringVar(value = "./text.txt")
-EntrySecret = ttk.Entry(master = FrameSecret, text = PathSecret)
-EntrySecret.pack(side = "top", padx = 25, fill = "x")
-FrameSecretButtons = ttk.Frame(master = FrameSecret)
-FrameSecretButtons.pack(side = "top", padx = 15, pady = 5, fill = "x")
-ButtonSecretLoad = ttk.Button(master = FrameSecretButtons,
-                              text = "Load secret from file",
-                              command = ButtonSecretLoadClick)
-ButtonSecretSave = ttk.Button(master = FrameSecretButtons,
-                              text = "Save secret to file",
-                              command = ButtonSecretSaveClick)
-ButtonSecretLoad.pack(side = "left", padx = 10, fill = "x", expand = True)
-ButtonSecretSave.pack(side = "right", padx = 10, fill = "x", expand = True)
-LabelSecretFeedback = ttk.Label(master = FrameSecret, text = "")
-LabelSecretFeedback.pack(side = "top", padx = 25, pady = 5, fill = "x")
-TextSecret = tk.Text(master = FrameSecret, width = 10)
-TextSecret.pack(side = "bottom", fill = "both", expand = True, padx = 25, pady = 10)
+LabelSecretCaption = ttk.Label(master=FrameSecret, text="Secret text")
+LabelSecretCaption.pack(side="top", pady=5)
+PathSecret = tk.StringVar(value="./text.txt")
+EntrySecret = ttk.Entry(master=FrameSecret, text=PathSecret)
+EntrySecret.pack(side="top", padx=25, fill="x")
+FrameSecretButtons = ttk.Frame(master=FrameSecret)
+FrameSecretButtons.pack(side="top", padx=15, pady=5, fill="x")
+ButtonSecretLoad = ttk.Button(master=FrameSecretButtons,
+                              text="Load secret from file",
+                              command=ButtonSecretLoadClick)
+ButtonSecretSave = ttk.Button(master=FrameSecretButtons,
+                              text="Save secret to file",
+                              command=ButtonSecretSaveClick)
+ButtonSecretLoad.pack(side="left", padx=10, fill="x", expand=True)
+ButtonSecretSave.pack(side="right", padx=10, fill="x", expand=True)
+LabelSecretFeedback = ttk.Label(master=FrameSecret, text="")
+LabelSecretFeedback.pack(side="top", padx=25, pady=5, fill="x")
+TextSecret = tk.Text(master=FrameSecret, width=10)
+TextSecret.pack(side="bottom", fill="both", expand=True, padx=25, pady=10)
 
-LabelModeCaption = ttk.Label(master = FrameMode, text = "Mode")
-LabelModeCaption.pack(side = "top", pady = 5)
-PathImage = tk.StringVar(value = "./image.bmp")
-EntryImage = ttk.Entry(master = FrameMode, text = PathImage)
-EntryImage.pack(side = "top", padx = 25, fill = "x")
-FrameImageButtons = ttk.Frame(master = FrameMode)
-FrameImageButtons.pack(side = "top", padx = 15, pady = 5, fill = "x")
-ButtonModeDisclose = ttk.Button(master = FrameImageButtons,
-                                text = "Disclose secret from image",
-                                width = 25,
-                                command = ButtonModeDiscloseClick)
-ButtonModeHide = ttk.Button(master = FrameImageButtons,
-                            text = "Hide secret in image",
-                            width = ButtonModeDisclose.cget("width"),
-                            command = ButtonModeHideClick)
-ButtonModeDisclose.pack(side = "right", padx = 10, fill = "x", expand = True)
-ButtonModeHide.pack(side = "left", padx = 10, fill = "x", expand = True)
-LabelModeFeedback = ttk.Label(master = FrameMode, text = "")
-LabelModeFeedback.pack(side = "top", padx = 25, pady = 5, fill = "x")
-TextMode = tk.Text(master = FrameMode, width = 10)
-TextMode.pack(side = "bottom", fill = "both", expand = True, padx = 25, pady = 10)
+LabelModeCaption = ttk.Label(master=FrameMode, text="Mode")
+LabelModeCaption.pack(side="top", pady=5)
+PathImage = tk.StringVar(value="./image.bmp")
+EntryImage = ttk.Entry(master=FrameMode, text=PathImage)
+EntryImage.pack(side="top", padx=25, fill="x")
+FrameImageButtons = ttk.Frame(master=FrameMode)
+FrameImageButtons.pack(side="top", padx=15, pady=5, fill="x")
+ButtonModeDisclose = ttk.Button(master=FrameImageButtons,
+                                text="Disclose secret from image",
+                                width=25,
+                                command=ButtonModeDiscloseClick)
+ButtonModeHide = ttk.Button(master=FrameImageButtons,
+                            text="Hide secret in image",
+                            width=ButtonModeDisclose.cget("width"),
+                            command=ButtonModeHideClick)
+ButtonModeDisclose.pack(side="right", padx=10, fill="x", expand=True)
+ButtonModeHide.pack(side="left", padx=10, fill="x", expand=True)
+LabelModeFeedback = ttk.Label(master=FrameMode, text="")
+LabelModeFeedback.pack(side="top", padx=25, pady=5, fill="x")
+TextMode = tk.Text(master=FrameMode, width=10)
+TextMode.pack(side="bottom", fill="both", expand=True, padx=25, pady=10)
 
-LabelImageHidingCaption = ttk.Label(master = FrameImage,
-                                    text = "Image containing the secret")
-LabelImageHidingCaption.pack(side = "top", pady = 5)
-LabelImageHiding = ttk.Label(master = FrameImage)
-LabelImageHiding.pack(side = "top", pady = 5, fill = "both", expand = True)
-LabelImageVirginCaption = ttk.Label(master = FrameImage,
-                                    text = "Virgin image")
-LabelImageVirginCaption.pack(side = "top", pady = 5)
-LabelImageVirgin = ttk.Label(master = FrameImage)
-LabelImageVirgin.pack(side = "top", pady = 5, fill = "both", expand = True)
+LabelImageHidingCaption = ttk.Label(master=FrameImage,
+                                    text="Image containing the secret")
+LabelImageHidingCaption.pack(side="top", pady=5)
+LabelImageHiding = ttk.Label(master=FrameImage)
+LabelImageHiding.pack(side="top", pady=5, fill="both", expand=True)
+LabelImageVirginCaption = ttk.Label(master=FrameImage,
+                                    text="Virgin image")
+LabelImageVirginCaption.pack(side="top", pady=5)
+LabelImageVirgin = ttk.Label(master=FrameImage)
+LabelImageVirgin.pack(side="top", pady=5, fill="both", expand=True)
 
 root.mainloop()
